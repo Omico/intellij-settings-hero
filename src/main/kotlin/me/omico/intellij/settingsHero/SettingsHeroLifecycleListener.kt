530c6af35ecdd06541fe7b85b2bd63f659c1c3fa
Omico <me@omico.icu>
2023-08-07 22:04:04 -0700

    Migrate to AppLifecycleListener

--- src/main/kotlin/me/omico/intellij/settingsHero/SettingsHeroLifecycleListener.kt
+++ src/main/kotlin/me/omico/intellij/settingsHero/SettingsHeroLifecycleListener.kt
@@ -3,39 +3,32 @@
 package me.omico.intellij.settingsHero
 
 import com.intellij.ide.AppLifecycleListener
-import com.intellij.ide.ApplicationInitializedListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.vcs.changes.ignore.cache.PatternCache
-import kotlinx.coroutines.CoroutineScope
 import me.omico.intellij.settingsHero.plugin.plugins
 import me.omico.intellij.settingsHero.profile.SettingsHeroProfileManager
 import me.omico.intellij.settingsHero.repository.localRepository
 import me.omico.intellij.settingsHero.repository.refreshLocalRepository
-import me.omico.intellij.settingsHero.utility.messageBus
 
-lateinit var defaultProject: Project
-    private set
-
-lateinit var patternCache: PatternCache
-    private set
-
-internal class SettingsHeroInitializer : ApplicationInitializedListener {
-    override suspend fun execute(asyncScope: CoroutineScope) {
+internal class SettingsHeroLifecycleListener : AppLifecycleListener {
+    override fun appFrameCreated(commandLineArgs: MutableList<String>) {
         defaultProject = ProjectManager.getInstance().defaultProject
         patternCache = PatternCache.getInstance(defaultProject)
         if (!settingsHeroSettings.enabled) return
         if (settingsHeroSettings.localRepositoryDirectory.isBlank()) return
         refreshLocalRepository()
         SettingsHeroProfileManager.load()
-        messageBus.connect().subscribe(
-            AppLifecycleListener.TOPIC,
-            object : AppLifecycleListener {
-                override fun appClosing(): Unit = saveAll()
-            },
-        )
     }
+
+    override fun appClosing(): Unit = saveAll()
 }
+
+internal lateinit var defaultProject: Project
+    private set
+
+internal lateinit var patternCache: PatternCache
+    private set
 
 internal fun saveAll() {
     if (!settingsHeroSettings.enabled) return
