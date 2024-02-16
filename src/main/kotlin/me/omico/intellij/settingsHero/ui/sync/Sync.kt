b98b88ce97620bfe40aaea49d00f027434d92b9b
Omico <me@omico.icu>
2024-02-16 08:29:33 -0800

    Fix hide "save to local" button

--- src/main/kotlin/me/omico/intellij/settingsHero/ui/sync/Sync.kt
+++ src/main/kotlin/me/omico/intellij/settingsHero/ui/sync/Sync.kt
@@ -1,14 +1,18 @@
-// Copyright 2023 Omico
+// Copyright 2023-2024 Omico
 // SPDX-License-Identifier: GPL-3.0-only
 package me.omico.intellij.settingsHero.ui.sync
 
 import com.intellij.ui.dsl.builder.Panel
 import me.omico.intellij.settingsHero.message
+import me.omico.intellij.settingsHero.ui.component.group
 import me.omico.intellij.settingsHero.ui.isSettingsHeroEnabledProperty
 
 internal fun Panel.operation() {
-    group(title = message("settingsHero.group.operation.title")) {
-        visibleIf(isSettingsHeroEnabledProperty)
-        saveToLocal()
-    }
+    group(
+        modifier = { visibleIf(isSettingsHeroEnabledProperty) },
+        title = message("settingsHero.group.operation.title"),
+        init = {
+            saveToLocal()
+        },
+    )
 }
